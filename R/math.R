7a1428caf1db3af1ef12c2dc0a81120d4d9da62b
Jameel Alsalam <alsalam.jameel@epa.gov>
2020-05-22 23:15:09 -0400

    namespace futzing

--- R/math.R
+++ R/math.R
@@ -1,6 +1,7 @@
 # Unary math and arithmetic:
 
 #' @export
+#' @import vctrs
 vec_math.sunits_rcrd <- function(.fn, .x, ...) {
 
   res <- vec_math_base(.fn, field(.x, "value"))
@@ -15,12 +16,35 @@ vec_math.sunits_rcrd <- function(.fn, .x, ...) {
   )
 }
 
+#' @export vec_arith.sunits_rcrd
+#' @method vec_arith sunits_rcrd
+#' @export
 vec_arith.sunits_rcrd <- function(op, x, y, ...) {
   UseMethod("vec_arith.sunits_rcrd", y)
 }
 
-vec_arith.sunits_rcrd <- function(op, x, y, ...) {
+#' @method vec_arith.sunits_rcrd default
+#' @export
+vec_arith.sunits_rcrd.default <- function(op, x, y, ...) {
   stop_incompatible_op(op, x, y)
+}
+
+#' @method vec_arith.sunits_rcrd numeric
+#' @export
+vec_arith.sunits_rcrd.numeric <- function(op, x, y, ...) {
+  new_sunits_rcrd(
+    vec_arith_base(op, field(x, "value"), y),
+    field(x, "units")
+  )
+}
+
+#' @method vec_arith.numeric sunits_rcrd
+#' @export
+vec_arith.numeric.sunits_rcrd <- function(op, x, y, ...) {
+  new_sunits_rcrd(
+    vec_arith_base(op, x, field(y, "value")),
+    field(y, "units")
+  )
 }
 
 # vec_arith.sunits_rcrd.sunits_rcrd <- function(op, x, y, ...) {
